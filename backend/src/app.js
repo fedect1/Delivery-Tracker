3fc6cd8147c5058d37da96d6af5e32a82c3fd0bc
Federico Andres Carrillo <136801767+fedect1@users.noreply.github.com>
2023-10-25 16:42:07 +0000

    Create an order Schema to validate the body of the request Post

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import crypto from 'crypto';
 import orders from './orders.json' assert { type: 'json' };
-
+import OrderSchema from './validation-schemas/orderSchema.js'
 
 const app = express();
+app.use(express.json());
 app.disable("x-powered-by");
 
 app.get("/", (req, res) => {
@@ -28,6 +30,22 @@ app.get("/orders/:id", (req, res) => {
     }
 });
 
+app.post("/orders", (req, res) => {
+    try{
+    const validatedOrder = OrderSchema.parse(req.body);
+    const id = crypto.randomBytes(16).toString("hex");
+    const order = { ...validatedOrder, id};
+    console.log(validatedOrder)
+    orders.push(order);
+    res.status(201).json(order);
+    } catch (error) {
+        if (error instanceof zod.ZodError) {
+            res.status(400).json({message: error.message})
+        } else {
+            res.status(500).json({message: "Internal server error"})
+        }   
+    }
+})
 
 
 
---
+++ backend/src/validation-schemas/orderSchema.js
@@ -0,0 +1,29 @@
+import zod from 'zod';
+
+const OrderSchema = zod.object({
+    customerInfo: zod.object({
+      name: zod.string(),
+      phone: zod.string(),
+      address: zod.string(),
+      email: zod.string().email(),
+    }),
+    orderDetails: zod.object({
+      items: zod.array(
+        zod.object({
+          itemName: zod.string(),
+          quantity: zod.number().int().positive(),
+          pricePerUnit: zod.number().positive(),
+        })
+      ),
+      totalPrice: zod.number().positive(),
+    }),
+    status: zod.string(),
+    statusUpdates: zod.array(
+      zod.object({
+        timestamp: zod.string().regex(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/), // This is a simple regex for the date-time format. Depending on your requirements, you might want a more sophisticated validation.
+        update: zod.string(),
+      })
+    ),
+});
+
+export default OrderSchema;
