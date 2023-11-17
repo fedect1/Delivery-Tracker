9b225d09b28b756df8ce32cc503b647d60d6bb11
Federico Andres Carrillo <136801767+fedect1@users.noreply.github.com>
2023-11-17 16:09:21 +0000

    Create Sign In and Sign out Page

--- frontend/src/DeliveryTrackerApp.jsx
+++ frontend/src/DeliveryTrackerApp.jsx
@@ -1,8 +1,10 @@
+import { AppRouter } from "./router"
+import { BrowserRouter } from "react-router-dom"
 
 export const DeliveryTrackerApp = () => {
   return (
-    <div>
-      Delivery Tracker App
-    </div>
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
   )
 }
---
+++ frontend/src/order/pages/OrderPage.jsx
@@ -0,0 +1,9 @@
+
+export const OrderPage = () => {
+  return (
+    <div>
+      Order page
+    </div>
+  )
+}
+
