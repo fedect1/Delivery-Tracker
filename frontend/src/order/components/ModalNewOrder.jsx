559af786ced2c5d061defef1441527871365a9e1
Federico Andres Carrillo <136801767+fedect1@users.noreply.github.com>
2023-11-20 16:28:22 +0000

    Add validation for empty fields in Create new order form

--- frontend/src/order/components/ModalNewOrder.jsx
+++ frontend/src/order/components/ModalNewOrder.jsx
@@ -1,5 +1,7 @@
 import Modal from 'react-modal'
 import { useState } from 'react'
+import Swal from 'sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css'
 
 const customStyles = {
     content: {
@@ -17,35 +19,62 @@ Modal.setAppElement('#root');
 export const ModalNewOrder = () => {
     const [modalIsOpen, setIsOpen] = useState(true);
 
+    const [formValues, setFormValues] = useState({
+        fullname: '',
+        address: '',
+        phone: '',
+        email: '',
+        itemname: '',
+        price: '',
+        quantity: '',
+        tracknumber: '',
+    });
+
+    const onInputChange = ({ target }) => {
+        setFormValues({
+            ...formValues,
+            [target.name]: target.value
+        });
+    }
+
     const onCloseModal = () => {
         console.log('onCloseModal')
         setIsOpen(false);
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log('onSubmit')
+        if (formValues.fullname === '' || formValues.address === '' || formValues.phone === '' || formValues.email === '' || formValues.itemname === '' || formValues.price === '' || formValues.quantity === '' || formValues.tracknumber === '') {
+            Swal.fire('Error', 'All fields are required', 'error');
+            return;
+        }
     }
 
   return (
     <Modal isOpen={ modalIsOpen } onRequestClose={ onCloseModal } style={ customStyles } className="modal" overlayClassName="modal-fondo" closeTimeoutMS={ 200 }>
       <h1> New Order </h1>
       <hr />
-      <form className="container">
+      <form className="container" onSubmit={ onSubmit }>
           <div className="form-group mb-2">
               <label>Costumer info</label>
-              <input className="form-control" placeholder="Fullname" />
-              <input className="form-control mt-2" placeholder="Address" />
-              <input className="form-control mt-2" placeholder="Phone" />
-              <input className="form-control mt-2" placeholder="Email" />
+              <input className="form-control" placeholder="Fullname" name='fullname' value={ formValues.fullname } onChange={ onInputChange }/>
+              <input className="form-control mt-2" placeholder="Address" name='address' value={formValues.address} onChange={onInputChange}/>
+              <input className="form-control mt-2" placeholder="Phone" name='phone' value={formValues.phone} onChange={onInputChange}/>
+              <input className="form-control mt-2" placeholder="Email" name='email' value={formValues.email} onChange={onInputChange}/>
           </div>
 
           <div className="form-group mb-2">
               <label>Order Details</label>
-              <input className="form-control" placeholder="Item name" />
-              <input className="form-control mt-2" placeholder="Price per item" />
-              <input className="form-control mt-2" placeholder="Quantity" />
+              <input className="form-control" placeholder="Item name" name='itemname' value={formValues.itemname} onChange={onInputChange}/>
+              <input className="form-control mt-2" placeholder="Price per item" name='price' value={formValues.price} onChange={onInputChange}/>
+              <input className="form-control mt-2" placeholder="Quantity" name='quantity' value={formValues.quantity} onChange={onInputChange}/>
           </div>
 
           <hr />
           <div className="form-group mb-3">
               <label>Track number</label>
-              <input className="form-control" placeholder="Track number" />
+              <input className="form-control" placeholder="Track number" name='tracknumber' value={formValues.tracknumber} onChange={onInputChange}/>
           </div>
 
           <button
