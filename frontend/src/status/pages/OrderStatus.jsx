20b3dd3158d0fdc162b55f223b384bf7eddfa690
Federico Andres Carrillo <136801767+fedect1@users.noreply.github.com>
2023-12-06 14:03:10 +0000

    Add goBack function to OrderStatus component

--- frontend/src/status/pages/OrderStatus.jsx
+++ frontend/src/status/pages/OrderStatus.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom"
 import { useGetOrderByTrackingNum } from "../../hooks/useGetOrderByTrackingNum"
 import { useEffect, useState } from "react"
+import { useNavigate } from 'react-router-dom';
 import capitalizeFirstLetter from "../../helpers/capitalizeFirstLetter"
 import dateSetter from "../../helpers/dateSetter"
 export const OrderStatus = () => {
@@ -8,6 +9,11 @@ export const OrderStatus = () => {
     const [order, setOrder] = useState({})
     const [loading, setLoading] = useState(true)
     const {orderId} = useParams()
+    const navigate = useNavigate();
+
+    const goBack = () => {
+      navigate(`/track-order`)
+    }
     useEffect(() => {
         const loadOrder = async () => {
           try {
@@ -32,23 +38,32 @@ export const OrderStatus = () => {
     }
 
   return (
-    <div className="card mt-5" style={{ maxWidth: '25rem', margin: 'auto' }}>
-      <div className="card-header text-center">Order: <span className="text-info">{orderId}</span></div>
-      <div className="card-body">
-        <h5 className="card-title text-center">{order.costumerInfo.name}</h5>
-        <p className="card-text text-center"><span className="text-secondary">Address:</span> {order.costumerInfo.address} </p>
-        <h4 className="card-title text-center">Current status: <span className="text-warning">{capitalizeFirstLetter(order.status)}</span></h4>
-        <div className="card-footer">
+    <div className="container">
+      <div className="text-end mt-3">
+        <button className="btn btn-outline-primary" onClick={goBack}>
+          <i className="fas fa-arrow-left"></i>
+          &nbsp;
+          Back  
+        </button>
+      </div>
+      <div className="card mt-5" style={{ maxWidth: '25rem', margin: 'auto' }}>
+        <div className="card-header text-center">Order: <span className="text-info">{orderId}</span></div>
+        <div className="card-body">
+          <h5 className="card-title text-center">{order.costumerInfo.name}</h5>
+          <p className="card-text text-center"><span className="text-secondary">Address:</span> {order.costumerInfo.address} </p>
+          <h4 className="card-title text-center">Current status: <span className="text-warning">{capitalizeFirstLetter(order.status)}</span></h4>
+          <div className="card-footer">
 
-          <ul className="list-group list-group-flush text-right">
+            <ul className="list-group list-group-flush text-right">
 
-          { order.statusUpdates && order.statusUpdates.map((statusUpdate, index) => {
-            return (
-              <li key={index} className="list-group-item"><span className="text-secondary">{dateSetter(statusUpdate.timestamp)}</span> {capitalizeFirstLetter(statusUpdate.update)}</li>
-              )        
-            })}
-          
-          </ul>
+            { order.statusUpdates && order.statusUpdates.map((statusUpdate, index) => {
+              return (
+                <li key={index} className="list-group-item"><span className="text-secondary">{dateSetter(statusUpdate.timestamp)}</span> {capitalizeFirstLetter(statusUpdate.update)}</li>
+                )        
+              })}
+            
+            </ul>
+          </div>
         </div>
       </div>
     </div>
